47e174f7bdaf4fe7e743599db9f855758bd093ec
some-du6e <103129948+some-du6e@users.noreply.github.com>
2023-04-24 19:46:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-var boilerplate = `
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>cool site</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-       `
-console.clear()
-console.log("mkay")
+function main() {
+    var iframe = document.getElementById('lal'); 
+    var site = prompt("site")
+    iframe.setAttribute('src', site);
+    
+}
+
+document.onload = main
